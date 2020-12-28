d16b5ca59627d0fbcbfde8f5746d9288e057b140
KLukas04 <krinkelukas@gmail.com>
2020-12-28 17:20:42 +0100

    try stackoverflow solution

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from db import Connection
 com = Communication("RPI", "192.168.2.156")
 water = Water(com)
 
-t_com = threading.Thread(target= com.server())
-t_water = threading.Thread(target=water.main())
+t_com = threading.Thread(target=com.server
+t_water = threading.Thread(target=water.main)
 
 if __name__ == '__main__':
     print("Starting")
