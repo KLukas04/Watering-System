05fcf73c23e5bc0f5123b5176112992a33b087b1
KLukas04 <krinkelukas@gmail.com>
2020-12-30 13:28:22 +0100

    small note

--- API/test.py
+++ API/test.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://127.0.0.1:5000/" #when from another device use: 192.168.2.156
 
 # DatumUhrzeit Temperatur Feuchtigkeit Regen
 response = requests.get(BASE) #+ "alldata/DatumUhrzeit"
