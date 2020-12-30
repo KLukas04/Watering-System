55aba480cbe621eb67143913ebcf17fd22d83839
Lukas <krinkelukas@gmail.com>
2020-12-30 13:29:08 +0100

    Fetching works finally

--- RaspberryPiControl/RaspberryPiControle/Sensor.swift
+++ RaspberryPiControl/RaspberryPiControle/Sensor.swift
@@ -10,11 +10,55 @@ import SwiftUI
 import SwiftUICharts
 
 struct SensorData: View {
+    @State private var temperaturen = [Temperatur]()
     var body: some View{
-        Text("Data")
+        
+        List(temperaturen){ temp in
+            Text("\(temp.Temperatur)")
+        }
+        .onAppear{
+            getJSONTemperatur{ (temp) in
+                temperaturen = temp
+            }
+        }
+    }
+    
+    func getJSONTemperatur(completion: @escaping ([Temperatur]) -> ()){
+        let urlString = "http://192.168.2.156:5000/alldata/Temperatur"
+        
+        if let url = URL(string: urlString){
+            URLSession.shared.dataTask(with: url){ data, res, err in
+                if let data = data{
+                    print("hey")
+                    
+                    let decoder = JSONDecoder()
+                    if let json = try? decoder.decode(Response.self, from: data){
+                        var temperaturen = [Temperatur]()
+                        for temp in json.Temperatur{
+                            let temperature = Temperatur(Temperatur: temp)
+                            temperaturen.append(temperature)
+                        }
+                        completion(temperaturen)
+                    }else{
+                        print("Cant decode")
+                    }
+                }
+            }.resume()
+        }else{
+            print("Error")
+        }
     }
     
 }
+
+struct Response: Codable {
+    let Temperatur: [Double]
+}
+struct Temperatur: Codable, Identifiable{
+    let id = UUID()
+    let Temperatur: Double
+}
+
 
 struct Sensor_Previews: PreviewProvider {
     static var previews: some View {
